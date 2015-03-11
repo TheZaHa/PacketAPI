b31f557ea0373f6ab1226faa15d2447833de0961
TheZaHa <thezaha@gmail.com>
2015-03-11 17:25:05 +0100

    Update Main.java

--- src/com/gmail/thezaha/Main.java
+++ src/com/gmail/thezaha/Main.java
@@ -37,11 +37,14 @@ public class Main extends JavaPlugin {
 		//new constructor and methods - Work In Progress, changes not have been introduced yet ...
 		
 		TabObject to = new TabObject(PacketPriority.NORMAL/*TabList Priority*/);
+		//TabObject newTabObject = new TabObject(PacketPriority.NORMAL/*TabList Priority*/, -1 /*Ping*/);
 		
 		/**add to TabList - now, expanded to more methods**/
-		to.addSlot("Slot 3")
-		to.setSlot(0, "Slot 1");
+		to.addSlot("Slot 1")
+		to.addSlot(1,"Slot 2")
+		to.setSlot(0, "Edited Slot 1");
 		TabObject.setTab(p, to);
+		//TabObject.newSetTab(p, to);
 		
 		if(args.length == 1) {
 			if(args[0].equalsIgnoreCase("create")) {
